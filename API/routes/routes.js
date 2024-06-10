4cde222438c3d1f0cbf08be075b54e9875f00049
jaalgori0 <00088022@uca.edu.sv>
2024-06-10 09:50:50 -0600

    add

--- API/routes/routes.js
+++ API/routes/routes.js
@@ -16,11 +16,6 @@ router.post('/postperson', async (req, res) => {
 
     try{
         const dataToSave = await data.save();
-
-        if(!dataToSave){
-            return res.status(404).json({error:"error saving"})
-        }
-
         res.status(200).json({"result": "ok"});
     } catch(error) {
         res.status(400).json({"message": error.message});
